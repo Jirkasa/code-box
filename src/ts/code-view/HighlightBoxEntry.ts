aea3286488da9d6742e3dca6d866913a34abb850
Jiří <satorajirka@seznam.cz>
2024-08-02 19:25:51 +0200

    write comments for HighlightBoxManager and HighlightBoxEntry classes

--- src/ts/code-view/HighlightBoxEntry.ts
+++ src/ts/code-view/HighlightBoxEntry.ts
@@ -1,10 +1,18 @@
 import HighlightBox from "./HighlightBox";
 import HighlightBoxManager from "./HighlightBoxManager";
 
+/** Used internally by CodeView to store highlight box. */
 class HighlightBoxEntry {
+    /** Highlight box. */
     public readonly highlightBox : HighlightBox;
+    /** Highlight box manager. */
     public readonly highlightBoxManager : HighlightBoxManager;
 
+    /**
+     * Creates new highlight box entry.
+     * @param highlightBox Highlight box.
+     * @param highlightBoxManager Highlight box manager.
+     */
     constructor(highlightBox : HighlightBox, highlightBoxManager : HighlightBoxManager) {
         this.highlightBox = highlightBox;
         this.highlightBoxManager = highlightBoxManager;
--- src/ts/code-view/HighlightBoxManager.ts
+++ src/ts/code-view/HighlightBoxManager.ts
@@ -1,12 +1,21 @@
+/** Manager of highlight box. */
 class HighlightBoxManager {
+    /** Function that is called when detach method is called. */
     public onDetach : (() => void) | null = null;
+    /** Function that is called when unlinkCodeView method is called. */
     public onUnlinkCodeView : (() => void) | null = null;
 
+    /**
+     * Detaches highlight box.
+     */
     public detach() : void {
         if (!this.onDetach) return;
         this.onDetach();
     }
 
+    /**
+     * Unlinks code view from highlight box.
+     */
     public unlinkCodeView() : void {
         if (!this.onUnlinkCodeView) return;
         this.onUnlinkCodeView();
