4b031002d0d9f78703aced07ce31d50e67fb52ef
Jiří <satorajirka@seznam.cz>
2024-08-14 22:07:15 +0200

    comment code for TreeNode

--- src/ts/utils/TreeNode.ts
+++ src/ts/utils/TreeNode.ts
@@ -1,7 +1,17 @@
+/**
+ * Represents node of tree.
+ * @template T Value of node.
+ */
 class TreeNode<T> {
+    /** Child nodes of node. */
     public readonly children = new Array<TreeNode<T>>();
+    /** Value of node. */
     public value : T;
 
+    /**
+     * Creates new tree node.
+     * @param value Value of node.
+     */
     constructor(value : T) {
         this.value = value;
     }
