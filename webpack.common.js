942a6a5dab83b9a80c7fe82515e6055adbd3f37e
Jiří <satorajirka@seznam.cz>
2025-06-13 22:12:08 +0200

    update documentation for CodeBoxCodeView.addHighlight method

--- webpack.common.js
+++ webpack.common.js
@@ -34,6 +34,7 @@ function createHtmlWebpackPluginsForPagesInFolder(folderName, version) {
 }
 
 const documentationPages = createHtmlWebpackPluginsForPagesInFolder("documentation");
+const documentationV12xPages = createHtmlWebpackPluginsForPagesInFolder("1.2.x/documentation", "1.2.x");
 const documentationV11xPages = createHtmlWebpackPluginsForPagesInFolder("1.1.x/documentation", "1.1.x");
 const documentationV10xPages = createHtmlWebpackPluginsForPagesInFolder("1.0.x/documentation", "1.0.x");
 
@@ -53,6 +54,17 @@ module.exports = {
         "documentation-virtual-code-box": "./js/documentation/virtual-code-box/main.js",
         "documentation-creators": "./js/documentation/creators/main.js",
         "documentation-other-components": "./js/documentation/other-components/main.js",
+        // VERSION 1.2.x
+        "examples-v12x": "./js/1.2.x/examples/main.js",
+        "documentation-getting-started-v12x": "./js/1.2.x/documentation/getting-started/main.js",
+        "documentation-styling-v12x": "./js/1.2.x/documentation/styling/main.js",
+        "documentation-code-view-v12x": "./js/1.2.x/documentation/code-view/main.js",
+        "documentation-code-box-v12x": "./js/1.2.x/documentation/code-box/main.js",
+        "documentation-tab-code-box-v12x": "./js/1.2.x/documentation/tab-code-box/main.js",
+        "documentation-project-code-box-v12x": "./js/1.2.x/documentation/project-code-box/main.js",
+        "documentation-virtual-code-box-v12x": "./js/1.2.x/documentation/virtual-code-box/main.js",
+        "documentation-creators-v12x": "./js/1.2.x/documentation/creators/main.js",
+        "documentation-other-components-v12x": "./js/1.2.x/documentation/other-components/main.js",
         // VERSION 1.1.x
         "examples-v11x": "./js/1.1.x/examples/main.js",
         "documentation-getting-started-v11x": "./js/1.1.x/documentation/getting-started/main.js",
@@ -144,8 +156,14 @@ module.exports = {
             inject: true
         }),
         new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "pages", "1.2.x", "examples", "index.ejs"),
+            chunks: ["style", "common", "examples-v12x"],
+            filename: `1.2.x/examples/index.html`,
+            inject: true
+        }),
+        new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "pages", "1.1.x", "examples", "index.ejs"),
-            chunks: ["style", "common", "examples-v10x"],
+            chunks: ["style", "common", "examples-v11x"],
             filename: `1.1.x/examples/index.html`,
             inject: true
         }),
@@ -156,6 +174,7 @@ module.exports = {
             inject: true
         }),
         ...documentationPages,
+        ...documentationV12xPages,
         ...documentationV11xPages,
         ...documentationV10xPages,
         new CopyPlugin({
